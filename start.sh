d8a8a0faaa7faa101d532ad2ba2c251b8abd9432
Sheng_L <87348555+Sheng2216@users.noreply.github.com>
2022-11-23 10:08:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,23 @@
 #!/bin/bash -e 
 
+do_get_SERVER_HOST() {
+   possible_HOST_IP=$(hostname -I | awk '{print $1}')
+   UDP_SERVER_HOST=$(whiptail --inputbox "Please configure the server host IP for the LoRaWAN UDP packet forwarder and TTS service. You can check the OLED screen on the enclosure to get the IP." 10 60 $possible_HOST_IP --title "Host IP" 3>&1 1>&2 2>&3)
+   exitstatus=$?
+   if [ $exitstatus = 0 ]; then
+      echo "Server Host is configured to: " $UDP_SERVER_HOST
+   else
+      echo "User selected Cancel."
+      exit 0
+   fi
+}
 
 COLOR_HEADER="\e[41m\e[97m" # white on red
 COLOR_SUMMARY="\e[1;32m" # bold green on black
 COLOR_ERROR="\e[31m" # red
 COLOR_END="\e[0m"
+
+do_get_SERVER_HOST
 
 # ----------------------------
 # Tasks
@@ -17,9 +30,9 @@ echo -e "${COLOR_SUMMARY}[1] Pulling remote images (it may take a while for the 
 docker compose up -d
 
 echo -e "${COLOR_SUMMARY}"
-echo -e "${COLOR_SUMMARY}[3] Web interface URLs${COLOR_END}"
+echo -e "${COLOR_SUMMARY}[2] Web interface URLs${COLOR_END}"
 echo "---------------------------------------------------------------------"
-echo "Node-RED URL : http://<host-ip>:1880/"
-echo "Grafana URL  : http://<host-ip>:3000/   (admin/changeme)"
+echo "Node-RED URL : http://${UDP_SERVER_HOST}:1880/"
+echo "Grafana URL  : http://${UDP_SERVER_HOST}:3000/   (admin/changeme)"
 echo "---------------------------------------------------------------------"
 echo -e "${COLOR_END}"
